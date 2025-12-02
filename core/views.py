ddb8be15a9c18bc998ffaa1bb43640fad0cfe235
hibiki <ashimine1184@gmail.com>
2025-12-02 11:54:54 +0000

    feat(core): implement Room list view and UI card layout

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Room
 
-# Create your views here.
+class RoomListView(ListView):
+    model = Room
+    template_name = 'room_list.html'
