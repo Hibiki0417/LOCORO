5e6f75d109efd2f7999cc1c619563dbf0e75d621
HIBIKI <ashimine1184@gmail.com>
2026-05-12 05:24:22 +0900

    chore(deploy): create superuser from env vars during build

--- build.sh
+++ build.sh
@@ -12,6 +12,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'locoro_app.settings')
 import django
 django.setup()
 
+from django.contrib.auth import get_user_model
 from django.core.management import call_command
 from django.db import connection
 
@@ -43,4 +44,18 @@ if table_exists('core_roomimage') and not migration_is_applied('core', '0004_add
     call_command('migrate', 'core', '0004_add_roomimage_model', fake=True)
 
 call_command('migrate')
+
+username = os.getenv('DJANGO_SUPERUSER_USERNAME')
+email = os.getenv('DJANGO_SUPERUSER_EMAIL')
+password = os.getenv('DJANGO_SUPERUSER_PASSWORD')
+
+if username and email and password:
+    User = get_user_model()
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username=username, email=email, password=password)
+        print(f'Superuser {username} created.')
+    else:
+        print(f'Superuser {username} already exists. Skipping creation.')
+else:
+    print('Superuser env vars are not fully set. Skipping superuser creation.')
 PY
